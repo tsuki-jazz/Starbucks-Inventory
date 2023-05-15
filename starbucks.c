fe97057c1f290a57666667826ff61547ef2abe00
MargaretAcheampong <117048397+tsuki-jazz@users.noreply.github.com>
2023-05-15 15:39:30 -0400

    added option to quit program

--- starbucks.c
+++ starbucks.c
@@ -2,18 +2,36 @@
 
 int main(){
 
-        //Title
+    int option;
+
     {
+        //Title
         printf("Starbucks Menu\n");
         printf("==============\n");
     }
-        //Menu Options
 
     {
+        //Menu Options
         printf("1. Drinks\n");
         printf("2. Food\n");
         printf("3. At Home\n");
-        printf("4. Merchandise\n");
+        printf("4. Merchandise\n\n");
     }
+    
+    {   
+        //Prompts user input for selecting a menu option
+        printf("Enter the number that that corresponds to the menu option.\n");
+        printf("Remember, press to 0 to quit.\n");
+        printf("Response: ");
+        scanf("%d", &option);
+    }
+
+    {   //Quits Program
+        if(option == 0)
+        {
+        exit(main);
+        }
+
+    } 
     return 0;
 }
