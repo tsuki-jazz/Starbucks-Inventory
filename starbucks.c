cd63e7b2e3f23b76f3ca31ea9135e0c6f58acd6a
MargaretAcheampong <117048397+tsuki-jazz@users.noreply.github.com>
2023-05-15 15:25:02 -0400

    displays title and menu options in output

---
+++ starbucks.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+        //Title
+    {
+        printf("Starbucks Menu\n");
+        printf("==============\n");
+    }
+        //Menu Options
+
+    {
+        printf("1. Drinks\n");
+        printf("2. Food\n");
+        printf("3. At Home\n");
+        printf("4. Merchandise\n");
+    }
+    return 0;
+}
