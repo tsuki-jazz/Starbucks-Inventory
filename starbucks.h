ce165841d1bbac95a05072fb127485d3c5ab8896
MargaretAcheampong <117048397+tsuki-jazz@users.noreply.github.com>
2023-05-15 19:17:42 -0400

    added header file

---
+++ starbucks.h
@@ -0,0 +1,9 @@
+   
+   #define PRODUCT_ID_SIZE 22
+   #define PRODUCT_PRICE 22
+
+    int option;
+    int productID[PRODUCT_ID_SIZE] = {478, 480, 479, 873068625, 481, 2123427, 406, 2122613,
+                         409, 410, 411, 2123158, 565, 2123279, 2122167, 2122164,
+                         407, 414, 413, 412, 408, 420};
+    double price[PRODUCT_PRICE] = {3.26};
